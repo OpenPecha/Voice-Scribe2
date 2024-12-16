c51bf532704e707a0e84cab0d77486b5ef28af4b
tenkus47 <tenkus47@gmail.com>
2024-12-16 11:03:00 +0530

    update

--- app/root.tsx
+++ app/root.tsx
@@ -29,7 +29,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta http-equiv="Permissions-Policy" content="microphone=(self)"></meta>
+        <meta httpEquiv="Permissions-Policy" content="microphone=(self)"></meta>
         <Meta />
         <Links />
       </head>
